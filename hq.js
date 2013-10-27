38a8596c82602e6f654de63c69ea2e2780c3a96a
krogebry <bryan_kroger@htc.com>
2013-10-27 03:09:31 +0000

    Tweaking bits.

--- hq.js
+++ hq.js
@@ -31,15 +31,50 @@ module.exports = function(app, db) {
             kaiju = docs[0];
             //console.log( kaiju );
 
-            var hit = (Math.random()*10)
-            if(kaiju.hp == null){
-              kaiju.hp = 100;
+            // Who hits first?
+            var jaeger_test = Math.random();
+            var kaiju_test = Math.random();
+
+            if(jaeger_test > kaiju_test){ // Jaeger hit's first.
+              var hit = (Math.random()*10)
+              var luck = Math.random();
+              if(luck >= 0.90){
+                console.log( "%s(%s) got a lucky hit on %s(%s) for %s", jaeger.name, jaeger.hp, kaiju.name, kaiju.hp, hit )
+                hit += (hit*2);
+              else if(luck >= 0.50 && luck < 0.90){
+                console.log( "%s(%s) got a good hit on %s(%s) for %s", jaeger.name, jaeger.hp, kaiju.name, kaiju.hp, hit )
+                hit += (hit*0.20);
+              else{ // You are not lucky.
+                console.log( "%s(%s) hit %s(%s) for %s", jaeger.name, jaeger.hp, kaiju.name, kaiju.hp, hit )
+              }
+              kaiju.hp -= hit;
+
+              // The response is a standard hit.
+              var hit = (Math.random()*10)
+              jaeger.hp -= hit;
+
+            }else{  // Kaiju hit's first.
+              var hit = (Math.random()*10)
+              var luck = Math.random();
+              if(luck >= 0.90){
+                console.log( "%s(%s) got a lucky hit on %s(%s) for %s", kaiju.name, kaiju.hp, jaeger.name, jaeger.hp, hit )
+                hit += (hit*2);
+              else if(luck >= 0.50 && luck < 0.90){
+                console.log( "%s(%s) got a good hit on %s(%s) for %s", kaiju.name, kaiju.hp, jaeger.name, jaeger.hp, hit )
+                hit += (hit*0.20);
+              else{ // You are not lucky.
+                console.log( "%s(%s) hit %s(%s) for %s", kaiju.name, kaiju.hp, jaeger.name, jaeger.hp, hit )
+              }
+              jaeger.hp -= hit;
+
+              // The response is a standard hit.
+              var hit = (Math.random()*10)
+              kaiju.hp -= hit;
+              console.log( "%s(%s) hit back %s(%s) for %s", kaiju.name, kaiju.hp, jaeger.name, jaeger.hp, hit )
             }
-            kaiju.hp -= hit;
-            console.log( "%s hit %s for %s", jaeger.name, kaiju.name, hit );
 
             db.collection( "kaiju" ).update({ _id: kaiju._id }, kaiju );
-            //console.log( kaiju );
+            db.collection( "jaeger" ).update({ _id: jaeger._id }, jaeger );
 
             var body = JSON.stringify({ kaiju: kaiju, jaeger: jaeger });
             res.setHeader('Content-Type', 'application/json');
